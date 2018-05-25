7b33f5297bdc8cbbaa89051e6266f78944c781d3
Nicola Onofri <nicola.onofri@gmail.com>
2018-05-25 18:40:02 +0200

    Started working on webpage, added root route, later will be creating form and working on user auth

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+dummy_data = [
+    {
+        'name': 'Nicola',
+        'surname': 'Onofri'
+    }, {
+        'name': 'Mario',
+        'surname': 'Rossi'
+    }
+]
 
+
+@app.route('/login')
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def login():
+    print('Hello')
+    return render_template('login.html', data=dummy_data, title='Login')
 
 
 if __name__ == '__main__':
